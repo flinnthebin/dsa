328f34594cb3c2fa2605a8dad41f98adc49def76
Chris Flinn <christopher.flinn@gmail.com>
2024-06-26 02:39:47 +1000

    morning

---
+++ arrays/maxones.cpp
@@ -0,0 +1,11 @@
+//  __  __               ____                                _   _              ___
+// |  \/  | __ ___  __  / ___|___  _ __  ___  ___  ___ _   _| |_(_)_   _____   / _ \ _ __   ___  ___
+// | |\/| |/ _` \ \/ / | |   / _ \| '_ \/ __|/ _ \/ __| | | | __| \ \ / / _ \ | | | | '_ \ / _ \/ __|
+// | |  | | (_| |>  <  | |__| (_) | | | \__ \  __/ (__| |_| | |_| |\ V /  __/ | |_| | | | |  __/\__ \
+// |_|  |_|\__,_/_/\_\  \____\___/|_| |_|___/\___|\___|\__,_|\__|_| \_/ \___|  \___/|_| |_|\___||___/
+//
+// Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+// Input: nums = [1,1,0,1,1,1]
+// Output: 3
+// Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
