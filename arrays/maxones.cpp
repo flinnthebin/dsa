7f8bc64d501862d9bdf03dcab6ce2fe9593d66cc
Chris Flinn <christopher.flinn@gmail.com>
2024-06-26 02:51:59 +1000

    main

--- arrays/maxones.cpp
+++ arrays/maxones.cpp
@@ -3,9 +3,46 @@
 // | |\/| |/ _` \ \/ / | |   / _ \| '_ \/ __|/ _ \/ __| | | | __| \ \ / / _ \ | | | | '_ \ / _ \/ __|
 // | |  | | (_| |>  <  | |__| (_) | | | \__ \  __/ (__| |_| | |_| |\ V /  __/ | |_| | | | |  __/\__ \
 // |_|  |_|\__,_/_/\_\  \____\___/|_| |_|___/\___|\___|\__,_|\__|_| \_/ \___|  \___/|_| |_|\___||___/
-//
+
 // Given a binary array nums, return the maximum number of consecutive 1's in the array.
 
+// Example 1:
 // Input: nums = [1,1,0,1,1,1]
 // Output: 3
 // Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+
+// Example 2:
+// Input: nums = [1,0,1,1,0,1]
+// Output: 2
+
+// Constraints
+
+// 1 <= nums.length <= 10^5
+// nums[i] is either 0 or 1
+
+#include <vector>
+#include <cassert>
+
+class Solution {
+    public:
+        auto findMaxConsecutiveOnes(std::vector<int>& nums) -> int {
+            return 0;
+    }
+};
+
+
+auto main() -> int {
+    Solution solution;
+
+    auto exvec_1 = std::vector{1,1,0,1,1,1};
+    auto exout_1 = int{3};
+
+    auto exvec_2 = std::vector{1,0,1,1,0,1};
+    auto exout_2 = int{2};
+
+    auto res_1 = solution.findMaxConsecutiveOnes(exvec_1);
+    auto res_2 = solution.findMaxConsecutiveOnes(exvec_2);
+
+    assert(exout_1 == res_1);
+    assert(exout_2 == res_2);
+}
